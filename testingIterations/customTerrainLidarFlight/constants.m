231cf0a7f549228df664c0ef376104a5986669f0
Jessica Guetre <jessicaguetre@gmail.com>
2024-01-10 17:08:31 -0500

    New search patterns

--- testingIterations/customTerrainLidarFlight/constants.m
+++ testingIterations/customTerrainLidarFlight/constants.m
@@ -1,7 +1,8 @@
 % Scenario constants.
 updateRate = 3; % in Hz
 simTime = 200;
-xlimitsScene = [0 200];
+% xlimitsScene = [0 200];
+xlimitsScene = [0 80];
 ylimitsScene = [0 200];
 zlimitsScene = [920 980];
 
@@ -13,10 +14,19 @@ sizeTarget = 2;
 
 % UAV platform constants
 maxVelocity = 15;
-initialVelocity = [sqrt(maxVelocity^(2)/2) sqrt(maxVelocity^(2)/2) 0];
-initialPosition = [mean(xlimitsScene) mean(ylimitsScene) 960];
-% initialPosition = [10 10 960];
+initialVelocity = [15 0 0];
+% initialVelocity = [sqrt(maxVelocity^(2)/2) sqrt(maxVelocity^(2)/2) 0];
+% initialPosition = [mean(xlimitsScene) mean(ylimitsScene) 960];
+initialPosition = [10 10 960];
 % initialPosition = [160 160 960];
+
+% Spiral search constants.
+maxSpeed = 15; % Maximum speed
+segmentLength = 20; % Initial length of each spiral segment
+segmentStep = 20; % Amount to increase the spiral length after each full cycle
+
+% Lawnmower search constants.
+rowHeight = 20;
 
 % Lidar constants
 maxRange = 20;
