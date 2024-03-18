ab73d6ff91817179a7c24b69ee4fb825d461ddb9
Jessica Guetre <jessicaguetre@gmail.com>
2024-03-18 10:23:43 -0400

    Changes

--- simulation/testScripts/testTerrains.m
+++ simulation/testScripts/testTerrains.m
@@ -1,11 +1,13 @@
 close all;
 terrainNum = 2;
-gridSize = [100, 100];
+gridSize = [500, 500];
 createFigure = true;
 
+row = 10;
+col = 10;
+targetRoi = [row, row + 1, col, col + 1, 0, 3];
+
 for terrainNum = 1:10
-    [featureVertices, probabilityGrid] = getScene(terrainNum, gridSize, createFigure, [10, 10]);
+    [featureVertices, probabilityGrid] = getScene(terrainNum, gridSize, createFigure, targetRoi);
     % [featureVertices, probabilityGrid] = getScene(selectedTerrainType, gridSize, createFigure);
 end
-
-% [featureVertices, probabilityGrid] = getScene(terrainNum, gridSize, createFigure, [10, 10]);
