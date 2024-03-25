09026c752ed472af5a3a29f382869d8c390e3248
Jessica Guetre <jessicaguetre@gmail.com>
2024-03-25 15:49:12 -0400

    Docstrings for getWaypoints

--- simulation/getWaypoints.m
+++ simulation/getWaypoints.m
@@ -1,4 +1,18 @@
 function waypointIndex = getWaypoints(flightType, gridSize, probabilityGrid, initialPosition, distanceFactor, targetRoi, createFigure)
+% Generates waypoints for UAV navigation based on specified flight patterns and terrain analysis.
+%
+% Inputs:
+% - flightType (string): Type of flight pattern to use ('probabilistic', 'parallelLine', 'spiral').
+% - gridSize (2-element vector): Size of the grid [rows, cols] over which the UAV operates.
+% - probabilityGrid (matrix): Grid of probabilities representing terrain features.
+% - initialPosition (3-element vector): The UAV's initial position [x, y, elevation].
+% - distanceFactor (scalar): Weighting factor for distance in probabilistic calculations.
+% - targetRoi (4-element vector): Region of interest [startRow, endRow, startCol, endCol] defining the target area.
+% - createFigure (boolean): Flag indicating whether to create visualizations of the operation.
+%
+% Output:
+% - waypointIndex (scalar): Index of the final waypoint reached in the navigation process.
+
     uavElevation = initialPosition(3);
     % maxNumWaypoints = gridSize(1) * gridSize(2) / 2;
     % waypoints = zeros(1, 3, maxNumWaypoints);
@@ -42,8 +56,8 @@ function waypointIndex = getWaypoints(flightType, gridSize, probabilityGrid, ini
         end
 
         if createFigure
-            [distanceGrid, successGrid] = calculateGrids(currentPosition, gridSize, probabilityGrid, visited, distanceFactor);
-            updateHeatmaps(successGrid, distanceGrid, gridSize);
+            [proximityGrid, successGrid] = calculateGrids(currentPosition, gridSize, probabilityGrid, visited, distanceFactor);
+            updateHeatmaps(successGrid, proximityGrid, gridSize);
         end
     end
 
@@ -55,21 +69,34 @@ function waypointIndex = getWaypoints(flightType, gridSize, probabilityGrid, ini
     end
 end
 
-function [distanceGrid, successGrid] = calculateGrids(currentPosition, gridSize, probabilityGrid, visited, distanceFactor)
-    distanceGrid = zeros(gridSize);
+function [proximityGrid, successGrid] = calculateGrids(currentPosition, gridSize, probabilityGrid, visited, distanceFactor)
+% Calculates distance and success grids based on UAV's current position, visited cells, and terrain probabilities.
+%
+% Inputs:
+% - currentPosition (2-element vector): Current [x, y] position of the UAV.
+% - gridSize (2-element vector): Size of the grid [rows, cols].
+% - probabilityGrid (matrix): Grid of probabilities representing terrain features.
+% - visited (matrix): Grid indicating whether cells have been visited.
+% - distanceFactor (scalar): Weighting factor for distance in the success calculation.
+%
+% Outputs:
+% - proximityGrid (matrix): Grid representing the normalized inverse distance to the current position.
+% - successGrid (matrix): Grid representing success probabilities of moving to each cell, factoring in distance and terrain probabilities.
+
+    proximityGrid = zeros(gridSize);
 
     for y = 1:gridSize(1)
         for x = 1:gridSize(2)
-            distanceGrid(y, x) = abs(currentPosition(2) - x) + abs(currentPosition(1) - y);
+            proximityGrid(y, x) = abs(currentPosition(2) - x) + abs(currentPosition(1) - y);
         end
     end
-    distanceGrid = 1 - (distanceGrid - min(distanceGrid(:))) / max(max(distanceGrid(:)) - min(distanceGrid(:)), 0.01); % normalize and invert
+    proximityGrid = 1 - (proximityGrid - min(proximityGrid(:))) / max(max(proximityGrid(:)) - min(proximityGrid(:)), 0.01); % normalize and invert
 
     successGrid = zeros(gridSize);
     for y = 1:gridSize(1)
         for x = 1:gridSize(2)
             if visited(y, x) == 0
-                successGrid(y, x) = distanceGrid(y, x) * distanceFactor + probabilityGrid(y, x);
+                successGrid(y, x) = proximityGrid(y, x) * distanceFactor + probabilityGrid(y, x);
             end
         end
     end
@@ -77,6 +104,19 @@ function [distanceGrid, successGrid] = calculateGrids(currentPosition, gridSize,
 end
 
 function proposedPath = findNextPositionProbabilistic(currentPosition, lidarRange, gridSize, successGrid, visited, uavElevation)
+% Determines the next position for the UAV based on a probabilistic analysis of the success grid.
+%
+% Inputs:
+% - currentPosition (2-element vector): Current [x, y] position of the UAV.
+% - lidarRange (scalar): Range of the UAV's LIDAR sensor.
+% - gridSize (2-element vector): Size of the grid [rows, cols].
+% - successGrid (matrix): Grid representing success probabilities.
+% - visited (matrix): Grid indicating whether cells have been visited.
+% - uavElevation (scalar): Elevation of the UAV.
+%
+% Output:
+% - proposedPath (matrix): Proposed path to the next position, considering terrain and visitation probabilities.
+
     [successPositions, successIndices] = maxk(successGrid(:), 5);
     [successY, successX] = ind2sub(size(successGrid), successIndices);
     bestVisitedCount = 0;
@@ -100,6 +140,15 @@ function proposedPath = findNextPositionProbabilistic(currentPosition, lidarRang
 end
 
 function [proposedPath, newDirection] = findNextPositionParallelLine(currentPosition, lidarRange, gridSize, lastDirection, uavElevation)
+% Calculates the next position for the UAV using a parallel line flight pattern.
+%
+% Inputs:
+% - currentPosition, lidarRange, gridSize, lastDirection, uavElevation: Same as in `findNextPositionProbabilistic`.
+%
+% Outputs:
+% - proposedPath (matrix): The proposed path for the UAV.
+% - newDirection (2-element vector): The new direction for the next segment of the flight.
+
     successY = currentPosition(1);
     successX = currentPosition(2);
 
@@ -122,6 +171,16 @@ function [proposedPath, newDirection] = findNextPositionParallelLine(currentPosi
 end
 
 function [proposedPath, newDirection, newSegmentLength] = findNextPositionSpiral(currentPosition, lidarRange, lastDirection, lastSegmentLength, uavElevation)
+% Determines the next position for the UAV using a spiral flight pattern.
+%
+% Inputs:
+% - currentPosition, lidarRange, gridSize (use gridSize from 'lastDirection' context), lastDirection, lastSegmentLength, uavElevation: Adapted for spiral path finding.
+%
+% Outputs:
+% - proposedPath (matrix): The proposed path for the UAV in a spiral pattern.
+% - newDirection (2-element vector): New direction for continuing the spiral.
+% - newSegmentLength (scalar): Length of the new segment in the spiral pattern.
+
     if isequal(lastDirection, [1, 0])
         newDirection = [0, 1];
         newSegmentLength = lastSegmentLength + lidarRange;
@@ -143,6 +202,20 @@ function [proposedPath, newDirection, newSegmentLength] = findNextPositionSpiral
 end
 
 function [proposedPath, visitedCount] = generatePath(currentPosition, successPosition, uavElevation, visited, gridSize, lidarRange)
+% Generates a path from the current position to a target position, considering UAV elevation and updating visited cells.
+%
+% Inputs:
+% - currentPosition (2-element vector): Current [x, y] position of the UAV.
+% - successPosition (2-element vector): Target [x, y] position for the UAV.
+% - uavElevation (scalar): Elevation of the UAV.
+% - visited (matrix): Grid indicating whether cells have been visited.
+% - gridSize (2-element vector): Size of the grid [rows, cols].
+% - lidarRange (scalar): Range of the UAV's LIDAR sensor.
+%
+% Outputs:
+% - proposedPath (matrix): Generated path from current to target position, with each row as [x, y, elevation].
+% - visitedCount (scalar): The number of cells newly marked as visited along the generated path.
+
     visitedCount = 1;
 
     if isequal(currentPosition, successPosition)
@@ -189,22 +262,31 @@ function [proposedPath, visitedCount] = generatePath(currentPosition, successPos
     end
 end
 
-function orientation = calculateOrientation(proposedPath, i)
-    if i == 1 % first waypoint, orientation unchanged
-        orientation = [1, 0, 0, 0]; % no rotation quaternion
-    else
-        deltaY = proposedPath(i, 1) - proposedPath(i-1, 1);
-        deltaX = proposedPath(i, 2) - proposedPath(i-1, 2);
-        yaw = atan2(deltaY, deltaX); % calculate yaw angle
-        w = cos(yaw / 2); % convert yaw angle to quaternion
-        x = 0; % no rotation around x-axis
-        y = 0; % no rotation around y-axis
-        z = sin(yaw / 2); % rotation around z-axis
-        orientation = [w, x, y, z];
-    end
-end
+% function orientation = calculateOrientation(proposedPath, i)
+%     if i == 1 % first waypoint, orientation unchanged
+%         orientation = [1, 0, 0, 0]; % no rotation quaternion
+%     else
+%         deltaY = proposedPath(i, 1) - proposedPath(i-1, 1);
+%         deltaX = proposedPath(i, 2) - proposedPath(i-1, 2);
+%         yaw = atan2(deltaY, deltaX); % calculate yaw angle
+%         w = cos(yaw / 2); % convert yaw angle to quaternion
+%         x = 0; % no rotation around x-axis
+%         y = 0; % no rotation around y-axis
+%         z = sin(yaw / 2); % rotation around z-axis
+%         orientation = [w, x, y, z];
+%     end
+% end
 
 function targetFound = checkTargetFound(visited, targetRoi)
+% Checks if the target region of interest has been found based on the visited grid.
+%
+% Inputs:
+% - visited (matrix): Grid indicating whether cells have been visited.
+% - targetRoi (4-element vector): Region of interest [startRow, endRow, startCol, endCol] defining the target area.
+%
+% Output:
+% - targetFound (boolean): Indicates whether any part of the target region has been visited.
+
     targetFound = false;
     for y = targetRoi(1):targetRoi(2)
         for x = targetRoi(3):targetRoi(4)
@@ -215,9 +297,19 @@ function targetFound = checkTargetFound(visited, targetRoi)
     end
 end
 
-function updateHeatmaps(successGrid, distanceGrid, gridSize)
+function updateHeatmaps(successGrid, proximityGrid, gridSize)
+% Updates visual heatmaps for the proximity grid and success grid.
+%
+% Inputs:
+% - successGrid (matrix): Grid representing success probabilities of moving to each cell.
+% - proximityGrid (matrix): Grid representing the normalized inverse distance to the current position.
+% - gridSize (2-element vector): Size of the grid [rows, cols].
+%
+% Behavior:
+% Creates and updates two figures with heatmaps for distance and success grids, enhancing visualization of the UAV's environment.
+
     figure(1); % update proximity heatmap
-    imagesc(distanceGrid);
+    imagesc(proximityGrid);
     colorbar;
     axis equal;
     title('Proximity Grid Heatmap');
@@ -238,6 +330,17 @@ function updateHeatmaps(successGrid, distanceGrid, gridSize)
 end
 
 function visited = updateVisitedFromLidar(currentPosition, visited, gridSize, lidarRange)
+% Updates the visited grid based on the UAV's current position and its LIDAR range.
+%
+% Inputs:
+% - currentPosition (2-element vector): Current [x, y] position of the UAV.
+% - visited (matrix): Grid indicating whether cells have been visited.
+% - gridSize (2-element vector): Size of the grid [rows, cols].
+% - lidarRange (scalar): Range of the UAV's LIDAR sensor.
+%
+% Output:
+% - visited (matrix): Updated visited grid, with cells within LIDAR range marked as visited.
+
     for y = 1:gridSize(1)
         for x = 1:gridSize(2)
             horizontalDistance = sqrt((currentPosition(2) - x)^2 + (currentPosition(1) - y)^2); % Corrected
